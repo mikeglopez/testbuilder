79858398f11a0aeb85a278d70077bd787a04b5a2
Mike Lopez <mikeglopez@me.com>
2018-10-05 22:49:35 -0500

    "Detect Visa and MasterCard"

--- detectNetwork.js
+++ detectNetwork.js
@@ -15,16 +15,22 @@ var detectNetwork = function(cardNumber) {
   // Once you've read this, go ahead and try to implement this function, then return to the console.
   var num = cardNumber.split('');
   var prefix = num.slice(0, 2).join('');
+  var len = num.length;
 
-  if (num.length === 14 && (prefix === '38' || prefix === '39')) {
+  if (len === 14 && (prefix === '38' || prefix === '39')) {
   	return `Diner's Club`;
   }
-  else if (num.length === 15 && (prefix === '34' || prefix === '37')) {
+  else if (len === 15 && (prefix === '34' || prefix === '37')) {
   	return `American Express`
+  }
+  else if (num[0] === '4' && (len === 13 || len === 16 || len === 19)) {
+  	return 'Visa';
+  }
+  else if ((prefix < 56 && prefix > 50) && len === 16) {
+  	return 'MasterCard';
   }
   else {
   	return `Error: Please enter a valid credit card number`;
   }
 };
-
 
